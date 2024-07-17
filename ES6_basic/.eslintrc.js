cc17de43c1d178bb6de8a1105c5116c31e7b97ca
niniisg <nisselsanchez@gmail.com>
2024-07-17 14:50:46 -0400

    eslint errro fixing

--- ES6_basic/.eslintrc.js
+++ ES6_basic/.eslintrc.js
@@ -1,37 +1,16 @@
-
 module.exports = {
-    env: {
-      browser: false,
-      es6: true,
-      jest: true,
+    "env": {
+        "browser": true,
+        "es6": true
     },
-    extends: [
-      'airbnb-base',
-      'plugin:jest/all',
-    ],
-    globals: {
-      Atomics: 'readonly',
-      SharedArrayBuffer: 'readonly',
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
     },
-    parserOptions: {
-      ecmaVersion: 2018,
-      sourceType: 'module',
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module"
     },
-    plugins: ['jest'],
-    rules: {
-      'no-console': 'off',
-      'no-shadow': 'off',
-      'no-restricted-syntax': [
-        'error',
-        'LabeledStatement',
-        'WithStatement',
-      ],
-    },
-    overrides:[
-      {
-        files: ['*.js'],
-        excludedFiles: 'babel.config.js',
-      }
-    ]
-  };
-  
+    "rules": {
+    }
+};

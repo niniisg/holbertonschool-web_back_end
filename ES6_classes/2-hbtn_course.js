4154a2ea052b2f9cb99a158926880163ab6594e9
niniisg <nisselsanchez@gmail.com>
2024-07-19 14:33:37 -0400

    UPdatetask 2

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,9 +1,9 @@
 export default class HolbertonCourse {
     constructor(name, length, students) {
-        if (typeof(name) !== 'string') {
+        if (typeof name !== 'string') {
             throw TypeError('Name should be a string');
         }
-        if (typeof(length) !== 'number') {
+        if (typeof length !== 'number') {
             throw TypeError('Length must be a number');
         }
 
@@ -19,7 +19,7 @@ export default class HolbertonCourse {
         return this._name;
     }
     set name(newName){
-        if (typeof(newName) !== 'string'){
+        if (typeof newName !== 'string'){
             throw TypeError('Name should be a string');
         }
         this._name = newName;
@@ -29,7 +29,7 @@ export default class HolbertonCourse {
         return this._lenght;
     }
     set length(newLenght){
-        if (typeof(newLength) !== 'number'){
+        if (typeof newLength !== 'number'){
             throw TypeError('Length must be a number');
         }
         this._length = newLenght;
@@ -40,7 +40,7 @@ export default class HolbertonCourse {
     }
 
     set students(newStudents){
-        if (Array.isArray(newStudents) !== true && typeof(newStudents[0] !== 'string')){
+        if (!Array.isArray(students) || students.some((student) => typeof student !== 'string')){
             throw TypeError('Students should be array of string');
         }
         this._students = newStudents;

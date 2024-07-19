61aef59f630d4e0659c869111ddb0adaa2461de2
niniisg <nisselsanchez@gmail.com>
2024-07-19 16:48:18 -0400

    fixing typo task 5

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,6 +1,6 @@
 export default class Building {
   constructor(sqft) {
-    if (this._constructer !== Building && !this.evacuationWariningMessage) {
+    if (this.constructer !== Building && !this.evacuationWariningMessage) {
       throw TypeError('Class extending Building must override evacuationWarningMessage');
     }
     this._sqft = sqft;

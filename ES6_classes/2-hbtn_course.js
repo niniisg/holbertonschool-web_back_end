bcf92a86626c09ee2dbae832445dc872e6df5105
niniisg <nisselsanchez@gmail.com>
2024-07-19 13:25:10 -0400

    adding task 2

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -13,10 +13,26 @@ class HolbertonCourse{
     get name(){
         return this._name;
     }
-    get name(){
+    set name(name){
+        if (typeof(name) !== 'string') throw TypeError('name should be a string');
+        this._name = name;
+    }
+    
+    get length(){
         return this._lenght;
     }
-    get name(){
+    set length(lenght){
+        if (typeof(length) !== 'lenght') throw TypeError('must be a Number');
+        this._length = lenght;
+    }
+
+    get students(){
         return this._students;
+    }
+
+    set students(students){
+        if (Array.isArray(students) !== true && typeof(students[0] !== 'string')){
+            throw TypeError('students should be array of string');
+        this._students = students;
     }
 }

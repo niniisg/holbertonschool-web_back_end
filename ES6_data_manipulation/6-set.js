c9c5e8fc94c16a48434c5a6dff2168133a3036cb
niniisg <nisselsanchez@gmail.com>
2024-07-23 19:57:58 -0400

    6-set.js

--- ES6_data_manipulation/6-set.js
+++ ES6_data_manipulation/6-set.js
@@ -1,4 +1,4 @@
-export default function setFromArray(array){
-    const set = new Set(array);
-    return set;
+export default function setFromArray(array) {
+  const set = new Set(array);
+  return set;
 }

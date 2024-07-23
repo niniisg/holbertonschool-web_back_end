03dabf87b2e12ef3f8afd01c1fdea46142ee1183
niniisg <nisselsanchez@gmail.com>
2024-07-23 19:27:35 -0400

    5-typed_arrays.js

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -2,7 +2,7 @@ export default function createInt8TypedArray(length, position, value) {
   const buffer = new ArrayBuffer(length);
   const view = new DataView(buffer);
 
-  if (position >= length) {
+  if (position > length) {
     throw new Error('position outside range');
   }
   view.setInt8(position, value);

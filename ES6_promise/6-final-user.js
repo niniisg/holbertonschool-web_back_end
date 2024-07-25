0f823df777d050b128368735d6e03eb43c59830f
niniisg <nisselsanchez@gmail.com>
2024-07-25 12:39:50 -0400

    adding task 6

---
+++ ES6_promise/6-final-user.js
@@ -0,0 +1,11 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((results) => results.map((result) => ({
+      status: result.status,
+      value: result.value === 'fulfilled' ? result.value : new Error(result.reason.message),
+
+    })));
+}

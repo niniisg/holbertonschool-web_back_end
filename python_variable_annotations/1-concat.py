96d1eddb10b5c15e527f9fa431fc8e24f420114a
niniisg <nisselsanchez@gmail.com>
2024-08-12 15:29:39 -0400

    adding task 1

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatenates two strings.
+
+    Args:
+        str1 (str): The first string.
+        str2 (str): The second string.
+
+    Returns:
+        str: The concatenated string.
+    """
+    return str1 + str2

18fa6e9d644421175a575074a150a293d977df84
niniisg <nisselsanchez@gmail.com>
2024-08-12 16:35:49 -0400

    adding task 4

---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Define an integer variable 'a' with a value of 1
+a: int = 1
+
+# Define a float variable 'pi' with a value of 3.14
+pi: float = 3.14
+
+# Define a boolean variable 'i_understand_annotations' with a value of True
+i_understand_annotations: bool = True
+
+# Define a string variable 'school' with a value of "Holberton"
+school: str = "Holberton"

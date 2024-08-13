e2b08a71c63db3be80e7f45a857caa689895317f
niniisg <nisselsanchez@gmail.com>
2024-08-13 15:07:14 -0400

    fixing pyco and documentation

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -11,6 +11,7 @@ def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
         v (Union[int, float]): The value, which can be an integer or a float.
 
     Returns:
-        Tuple[str, float]: A tuple where the first element is the key and the second element is the square of the value as a float.
+        Tuple[str, float]: A tuple where the first element is the key
+        and the second element is the square of the value as a float.
     """
     return (k, float(v**2))

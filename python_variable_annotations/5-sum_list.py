c3e3961a620cafe4648af86798286f590001799a
niniisg <nisselsanchez@gmail.com>
2024-08-13 14:12:20 -0400

    fixing pyco

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
-
-""" 
+"""
 this is a function that takes a list of floats as input
 """
 from typing import List
@@ -9,7 +8,6 @@ from typing import List
 def sum_list(input_list: List[float]) -> float:
     """
     sum list of floating point numbers
-
 
     :arg input_list: the list of float numbers
     :return: returns the sum of float numbers in a list

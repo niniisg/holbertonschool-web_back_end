3c6785f818996f17c1330201631a69fb65753c0a
niniisg <nisselsanchez@gmail.com>
2024-08-14 16:00:12 -0400

    fixing missing await

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -2,28 +2,29 @@
 
 import time
 import asyncio
+
 """
 this module measures the runtime of
-a async comprehension
+a async function
 """
 
 wait_n = __import__("1-concurrent_coroutines").wait_n
 
 
-def measure_time(n: int, max_delay: int) -> float:
+async def measure_time(n: int, max_delay: int) -> float:
     """
     Measures the total execution time
-    
+    for an async function
+
     Args:
         n (int): The number of times to call wait_n.
-        max_delay (int): The maximum delay
-        for each call to wait_n.
+        max_delay (int): The maximum delay for each call to wait_n.
 
     Returns:
         float: The average time per call to wait_n.
     """
     start_time = time.perf_counter()
-    asyncio.run(wait_n(n, max_delay))
+    await wait_n(n, max_delay)
     end_time = time.perf_counter()
     elapsed_time = end_time - start_time
     return elapsed_time / n

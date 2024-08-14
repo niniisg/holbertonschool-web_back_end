24612d29c230fdcfc8e8737a44843e71fa4c87ce
niniisg <nisselsanchez@gmail.com>
2024-08-14 14:42:24 -0400

    fixing pycodestile

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -2,7 +2,8 @@
 
 import time
 
-wait_n = __import__('1-concurrent_coroutines').wait_n
+wait_n = __import__("1-concurrent_coroutines").wait_n
+
 
 def measure_time(n: int, max_delay: int) -> float:
     """

53dd48cc111af18da716997447adda1cc36bf855
niniisg <nisselsanchez@gmail.com>
2024-08-14 15:38:20 -0400

    refactor: convert measure_time function to synchronous

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -2,7 +2,6 @@
 
 import time
 import asyncio
-
 """
 this module measures the runtime of
 a async comprehension
@@ -11,20 +10,20 @@ a async comprehension
 wait_n = __import__("1-concurrent_coroutines").wait_n
 
 
-async def measure_time(n: int, max_delay: int) -> float:
+def measure_time(n: int, max_delay: int) -> float:
     """
     Measures the total execution time
-
+    
     Args:
         n (int): The number of times to call wait_n.
-        max_delay (int): The maximum delay for each call to wait_n.
+        max_delay (int): The maximum delay
+        for each call to wait_n.
 
     Returns:
         float: The average time per call to wait_n.
     """
-
     start_time = time.perf_counter()
-    await wait_n(n, max_delay)
+    asyncio.run(wait_n(n, max_delay))
     end_time = time.perf_counter()
     elapsed_time = end_time - start_time
     return elapsed_time / n

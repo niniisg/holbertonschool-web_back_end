7443412d08e005fad0181a250bc9546043da428f
niniisg <nisselsanchez@gmail.com>
2024-08-20 21:28:35 -0400

    fixing documentation

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -6,12 +6,12 @@ using async comprohension
 import asyncio
 from typing import List
 
-async_generator = __import__('0-async_generator').async_generator
-"""
-this coroutine collects numbers from async_generator
-function using async_comprehension
-"""
+async_generator = __import__("0-async_generator").async_generator
 
 
 async def async_comprehension() -> List[float]:
+    """
+    this coroutine collects numbers from async_generator
+    function using async_comprehension
+    """
     return [i async for i in async_generator()]

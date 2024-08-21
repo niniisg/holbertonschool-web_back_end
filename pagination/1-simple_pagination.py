cd6d9d146a5d0741d9d6d646f0066aad5c943fc1
niniisg <nisselsanchez@gmail.com>
2024-08-21 15:56:17 -0400

    fixing error in code

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -22,9 +22,13 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        assert page > 0 or isinstance(page, int)
+        """
+        retrives a page data from the dataset.
+        """
+        assert isinstance(page, int) and page > 0  
         assert isinstance(page_size, int) and page_size > 0
         start, end = self.index_range(page, page_size)
+        
         dataset = self.dataset()
         if start >= len(dataset):
             return []

3ae497f31d5952d598895bf3235cb11ef207daee
niniisg <nisselsanchez@gmail.com>
2024-08-22 14:29:51 -0400

    adding task 11

---
+++ NoSQL/11-schools_by_topic.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+this module returns the list
+of school with specific topic
+"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """
+    Queries the mongo_collection to find
+    all schools that have the specified topic.
+    """
+    return mongo_collection.find( {'topics': 'topic'} )

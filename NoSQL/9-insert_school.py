ee026b08fd84cbc2468d59a81def3580f11a71ed
niniisg <nisselsanchez@gmail.com>
2024-08-22 12:27:19 -0400

    adding task 9

---
+++ NoSQL/9-insert_school.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+"""
+this module inserts a new document
+in a collection
+"""
+
+def insert_school(mongo_collection, **kwargs):
+    return mongo_collection.insert_one(kwargs).inserted_id

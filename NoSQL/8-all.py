ff0ac1ff09946865e80dd63474dfa241084309b8
niniisg <nisselsanchez@gmail.com>
2024-08-22 12:11:30 -0400

    fixing pyco

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python3
+"""
+this module list all document
+in a collection
+"""
+
 
 def list_all(mongo_collection):
+    """
+     Lists all documents in a MongoDB collection.
+    """
     return list(mongo_collection.find())
-

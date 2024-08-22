26253df9515855c8197e48f32b87cb5c367cda93
niniisg <nisselsanchez@gmail.com>
2024-08-21 21:46:04 -0400

    adding documentation

--- pagination/3-hypermedia_del_pagination.py
+++ pagination/3-hypermedia_del_pagination.py
@@ -38,6 +38,10 @@ class Server:
         return self.__indexed_dataset
 
     def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
+        """
+        this returns a dictionary of a
+        pagination data
+        """
         assert isinstance(index, int) and index >= 0
         assert isinstance(page_size, int) and page_size > 0
 

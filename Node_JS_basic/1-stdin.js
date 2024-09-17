bcff7db4d3ff9405e0dc24d333fd6b0c018c67dc
niniisg <nisselsanchez@gmail.com>
2024-09-17 13:55:11 -0400

    adding task1

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,13 @@
+console.log('Welcome to Holberton School, what is your name?');
+
+process.stdin.setEncoding('utf8');
+
+process.on('readable', () => {
+  const chunk = process.stdin.read();
+  if (chunk !== null) {
+    process.stdout.write(`Your name is: ${chunk}`);
+  }
+});
+process.stdin.on('end', () => {
+  console.log('This important software is now closing');
+});

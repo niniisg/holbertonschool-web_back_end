3f489757072d0cc7baaf0962d374e31aad3ffce4
niniisg <nisselsanchez@gmail.com>
2024-09-17 14:10:37 -0400

    fixing code error

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -2,7 +2,7 @@ console.log('Welcome to Holberton School, what is your name?');
 
 process.stdin.setEncoding('utf8');
 
-process.on('readable', () => {
+process.stdin.on('readable', () => {
   const chunk = process.stdin.read();
   if (chunk !== null) {
     process.stdout.write(`Your name is: ${chunk}`);

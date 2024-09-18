6b37e5603eefe7977edb8269a56225119fc2d551
niniisg <nisselsanchez@gmail.com>
2024-09-18 14:13:04 -0400

    Refactor code to trim rows before splitting in 2-read_file.js

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -7,7 +7,8 @@ function countStudents(path) {
 
     const data = fs.readFileSync(path,
       { encoding: 'utf8', flag: 'r' });
-    const rows = data.split('\n');
+
+    const rows = data.trim().split('\n');
 
     for (let i = 1; i < rows.length; i += 1) {
       const row = rows[i].split(',');
@@ -15,11 +16,12 @@ function countStudents(path) {
       if (row[3] === 'CS') listCS.push(row[0]);
       else if (row[3] === 'SWE') listSWE.push(row[0]);
     }
+
     console.log(`Number of students: ${rows.length - 1}`);
-    console.log(`Number of students in CS: ${listCS.length} List: ${listCS.join(', ')}`);
+    console.log(`Number of students in CS: ${listCS.length}. List: ${listCS.join(', ')}`);
     console.log(`Number of students in SWE: ${listSWE.length}. List: ${listSWE.join(', ')}`);
   } catch (err) {
-    throw new Error('cannot load the database');
+    throw new Error('Cannot load the database');
   }
 }
 

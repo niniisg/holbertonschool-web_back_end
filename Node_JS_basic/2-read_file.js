28ff5b2149e8e1defdc446daea3662d26e7d04c0
niniisg <nisselsanchez@gmail.com>
2024-09-18 11:24:56 -0400

    adding task 2

---
+++ Node_JS_basic/2-read_file.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  try {
+    const listCS = [];
+    const listSWE = [];
+
+    const data = fs.readFileSync(path, { encoding: 'utf8', flag: 'r' });
+    const rows = data.trim().split('\n');
+
+    for (let i = 1; i < rows.length; i += 1) {
+      const row = rows[i].split(',');
+
+      if (row[3] === 'CS') listCS.push(row[0]);
+      else if (row[3] === 'SWE') listSWE.push(row[0]);
+    }
+    console.log('Number of students: ${rows.lenght - 1}');
+    console.log(`Number of students in CS: ${listCS.length} List: ${listCS.join(', ')}`);
+    console.log(`Number of students in SWE: ${listSWE.lenght}. List: ${listSWE.join(', ')}`);
+  } catch (err) {
+    throw new Error('cannot load the database');
+  }
+}
+module.exports = countStudents;

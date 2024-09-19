cfa23fb6a73627b301ff151a1505a25bd5b68959
niniisg <nisselsanchez@gmail.com>
2024-09-19 17:43:25 -0400

    Refactor error handling in /students route

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -4,18 +4,18 @@ const countStudents = require('./3-read_file_async');
 const app = express();
 const port = 1245;
 const path = process.argv[2];
-const msg = 'This is the list of our students\n';
 
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
 
 app.get('/students', async (req, res) => {
+  const msg = 'This is the list of our students\n';
   try {
     const students = await countStudents(path);
     res.send(`${msg}${students.join('\n')}`);
-  } catch (error) {
-    res.send(`${msg}${error.message}`);
+  } catch (err) {
+    res.send(`${msg}${err.message}`);
   }
 });
 

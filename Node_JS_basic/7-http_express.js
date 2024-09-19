2fb5ef8c8b53733b4af91a7f85f7c42a4169a30e
niniisg <nisselsanchez@gmail.com>
2024-09-19 17:34:55 -0400

    fixing error

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -3,21 +3,22 @@ const countStudents = require('./3-read_file_async');
 
 const app = express();
 const port = 1245;
+const path = process.argv[2];
+const msg = 'This is the list of our students\n';
 
 app.get('/', (req, res) => {
-    res.send('Hello Holberton School!');
+  res.send('Hello Holberton School!');
 });
 
-app.get('/students', async(req, res) =>{
-    try {
-        const data = await countStudents(process.argv[2]);
-        res.send(`This is the list of our students\n${data}`);
-    } catch (error) {
-        res.send('This is the list of our students\n${error.message}');
-    }
+app.get('/students', async (req, res) => {
+  try {
+    const students = await countStudents(path);
+    res.send(`${msg}${students.join('\n')}`);
+  } catch (error) {
+    res.send(`${msg}${error.message}`);
+  }
 });
 
-app.listen(port, () => {
-    console.log('Server listening on port ${port}');
-});
+app.listen(port);
+
 module.exports = app;

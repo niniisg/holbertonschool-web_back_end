dbd86d8f72f2e6e79057aa8a2bf68114193caefe
niniisg <nisselsanchez@gmail.com>
2025-01-27 19:35:13 -0400

    Basic dictionary

---
+++ caching/0-basic_cache.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+BaseCaching module
+"""
+
+from base_caching import BaseCaching
+
+
+class BaseCaching(BaseCaching):
+    """
+    a class that manage a cache, Inherits
+    from BaseCaching
+    """
+
+    def put(self, key, item):
+        """
+        adds an item to cache if both key
+        and  item are not None
+        """
+        if key is None and item is None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        Retrieves an item from cache by key.
+        Args:
+            key: the key to look for in the cache
+        Returns:
+            the value for key if it exists, otherwise None.
+        """
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]

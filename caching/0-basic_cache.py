59c57318c8724832790545dc0b0f17fb5ed75968
niniisg <nisselsanchez@gmail.com>
2025-01-27 22:32:08 -0400

    fixing typo

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -1,13 +1,10 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """
 BaseCaching module
 """
-
-
 from base_caching import BaseCaching
 
-
-class BaseCaching(BaseCaching):
+class BaseCache(BaseCaching):
     """
     a class that manage a cache, Inherits
     from BaseCaching

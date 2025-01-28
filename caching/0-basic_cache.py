e98785cf5d6c1f27ae775e787de90057ea0fbb72
niniisg <nisselsanchez@gmail.com>
2025-01-27 21:49:11 -0400

    fixing mising file

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -1,9 +1,8 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """
 BaseCaching module
 """
-
-from base_caching import BaseCaching
+from base_caching import BaseCaching # type: ignore
 
 
 class BaseCaching(BaseCaching):

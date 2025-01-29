51bea6e6d8609510d2c5418ad55b3d467e8e6796
niniisg <nisselsanchez@gmail.com>
2025-01-29 19:53:23 -0400

    fixing documentation

--- caching/3-lru_cache.py
+++ caching/3-lru_cache.py
@@ -1,20 +1,26 @@
 #!/usr/bin/python3
 """ LRUCache module
-    Implements an LRU (Least Recently Used) caching system.
 """
 from base_caching import BaseCaching
 
 
 class LRUCache(BaseCaching):
-    """LRUCache: A class implementing an LRU caching system"""
+    """LRU caching system"""
 
     def __init__(self):
-        """Initializes the LRUCache with an empty order list."""
+        """
+        Initializes the LRUCache with
+        an empty order list
+        """
         super().__init__()
         self.order = []
 
     def put(self, key, item):
-        """Adds an item to the cache. If the cache is full, discards the least recently used item."""
+        """
+        Adds an item to the cache.
+        If the cache is full,
+        discards the least recently used item.
+        """
         if key is not None and item is not None:
             if key in self.cache_data:
                 self.order.remove(key)
@@ -26,10 +32,12 @@ class LRUCache(BaseCaching):
             self.order.append(key)
 
     def get(self, key):
-        """Retrieves an item from the cache, marking it as recently used."""
+        """
+        Retrieves an item from the cache,
+        marking it as recently used.
+        """
         if key in self.cache_data:
             self.order.remove(key)
             self.order.append(key)
             return self.cache_data[key]
         return None
-    

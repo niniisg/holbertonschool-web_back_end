ebbb564a373be9bd40831290ebb22b02fd19a22f
niniisg <nisselsanchez@gmail.com>
2025-02-14 14:24:21 -0400

    task 0

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -6,7 +6,6 @@ Regex-ing
 
 import re
 
-
 """
 module for readacting sensitive
 information in log messages
@@ -14,7 +13,15 @@ information in log messages
 
 
 def filter_datum(fields, redaction, message, separator):
+    """
+    Args:
+        This function takes a list of fields,
+        redacts their values in a given log message
+    Returns:
+        A string with the specified fields redacted.
+    """
     return re.sub(
         rf'({"|".join(fields)})=[^{separator}]*',
-        lambda m: f'{m.group(1)}={redaction}', message
+        lambda m: f"{m.group(1)}={redaction}",
+        message,
     )

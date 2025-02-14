825e73e99938a68d2e497895c92ab18372a9a215
niniisg <nisselsanchez@gmail.com>
2025-02-14 14:00:56 -0400

    reducing code lines

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -13,11 +13,8 @@ information in log messages
 """
 
 
-import re
-
 def filter_datum(fields, redaction, message, separator):
     return re.sub(
         rf'({"|".join(fields)})=[^{separator}]*',
-        lambda m: f'{m.group(1)}={redaction}',
-        message
+        lambda m: f'{m.group(1)}={redaction}', message
     )

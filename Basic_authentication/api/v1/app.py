20402797480b8f0c71e2ce40af2db09c52aa9593
niniisg <nisselsanchez@gmail.com>
2025-02-19 17:22:30 -0400

    fixing typos

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -5,7 +5,7 @@ Route module for the API
 from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
-from flask_cors import (CORS, cross_origin)
+from flask_cors import CORS, cross_origin
 import os
 
 
@@ -16,9 +16,9 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 @app.errorhandler(404)
 def not_found(error) -> str:
-    """ Not found handler
-    """
+    """Not found handler"""
     return jsonify({"error": "Not found"}), 404
+
 
 @app.errorhandler(401)
 def not_found(error) -> str:
@@ -27,8 +27,8 @@ def not_found(error) -> str:
     """
     return jsonify({"error": "Unauthorized"}), 401
 
+
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
     app.run(host=host, port=port)
-

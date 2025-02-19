cfb50f639943e2a4c1a19a58f24de5adb9023aaf
niniisg <nisselsanchez@gmail.com>
2025-02-19 17:18:19 -0400

    unauthorized

--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -25,7 +25,7 @@ def stats() -> str:
     stats['users'] = User.count()
     return jsonify(stats)
 
-@app_views.route('/status', methods=['GET'])
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
 def unauthorized() -> str:
     """ GET /api/v1/unauthorized
     Return:

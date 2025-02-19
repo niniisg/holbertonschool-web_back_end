d971212157698304689bdc7e8b88fcc9fca948bb
niniisg <nisselsanchez@gmail.com>
2025-02-19 17:35:42 -0400

    forbidden handler

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -27,10 +27,10 @@ def not_found(error) -> str:
     """
     return jsonify({"error": "Unauthorized"}), 401
 
+
 @app.errorhandler(403)
 def not_found(error) -> str:
-    """
-    """
+    """ """
     return jsonify({"error": "Forbidden"}), 403
 
 

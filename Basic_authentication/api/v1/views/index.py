339f3b4c9b900b0710b8b76f0df5c5637f954371
niniisg <nisselsanchez@gmail.com>
2025-02-19 17:33:57 -0400

    Error handler: Forbidden

--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -35,3 +35,11 @@ def unauthorized() -> str:
 
     """
     abort(401)
+
+@app_views.route("/forbidden", methods=["GET"], strict_slashes=False)
+def forbidden() -> str:
+    """GET /api/v1/forbidden
+    Return:
+      -raise a 403 error
+    """
+    abort(403)

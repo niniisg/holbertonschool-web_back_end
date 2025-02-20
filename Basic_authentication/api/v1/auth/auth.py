7d60c9d9674d6a3838dbd0d32608b4d5987a5fdd
niniisg <nisselsanchez@gmail.com>
2025-02-20 14:51:02 -0400

    request validation

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -40,7 +40,11 @@ class Auth:
         Returns:
             None
         """
-        return None
+        if request is None:
+            return None
+        if "Authorization" not in request.headers:
+            return None
+        return request.headers.get("Authorization")
 
     def current_user(self, request=None) -> TypeVar("User"):
         """

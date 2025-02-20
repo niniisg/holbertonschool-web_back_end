7e2aa0da0dc1d5e7ab7b4a59e703eb2844c2e8d5
niniisg <nisselsanchez@gmail.com>
2025-02-19 22:16:29 -0400

    task 4

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -18,7 +18,19 @@ class Auth:
         Returns:
             False
         """
-        return False
+        if path is None:
+            return True
+        if excluded_paths is None or path not in excluded_paths == []:
+            return True
+
+        if not path.endswith("/"):
+            path += "/"
+
+        for excluded_path in excluded_paths:
+            if excluded_path.endswith("/") and path == excluded_path:
+                return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
         """ "

48653d18a008ae0ea8a673522995c81cbc7dd80a
niniisg <nisselsanchez@gmail.com>
2025-02-21 17:21:43 -0400

    fixing pycodestyle

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -31,7 +31,8 @@ def before_request():
     """Handler for filtering each request before processing"""
     if auth is None:
         return
-    excluded_paths = ["/api/v1/status/", "/api/v1/unauthorized/", "/api/v1/forbidden/"]
+    excluded_paths = ["/api/v1/status/", "/api/v1/unauthorized/",
+                      "/api/v1/forbidden/"]
     if not auth.require_auth(request.path, excluded_paths):
         return
     if auth.authorization_header(request) is None:

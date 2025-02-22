9b2fd0aefdb7359746171035f6405d3cfe91af01
niniisg <nisselsanchez@gmail.com>
2025-02-21 20:42:22 -0400

    fixing typo

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -54,9 +54,6 @@ class BasicAuth(Auth):
         if decoded_base64_authorization_header is None:
             return None, None
 
-        if not isinstance(decoded_base64_authorization_header, str):
-            return None, None
-
         if ":" not in decoded_base64_authorization_header:
             return None, None
 

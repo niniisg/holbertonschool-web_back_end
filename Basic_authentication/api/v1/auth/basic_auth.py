16e2099460b83d32051d2bc54f65b9a39db6be11
niniisg <nisselsanchez@gmail.com>
2025-02-21 21:26:04 -0400

    Basic-Overload

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -98,15 +98,18 @@ class BasicAuth(Auth):
         if auth_header is None:
             return None
 
-        base64_auth_header = self.extract_base64_authorization_header(auth_header)
+        base64_auth_header = self.extract_base64_authorization_header(
+            auth_header)
         if base64_auth_header is None:
             return None
 
-        decoded_auth_header = self.decode_base64_authorization_header(base64_auth_header)
+        decoded_auth_header = self.decode_base64_authorization_header(
+            base64_auth_header)
         if decoded_auth_header is None:
             return None
 
-        user_email, user_pwd = self.extract_user_credentials(decoded_auth_header)
+        user_email, user_pwd = self.extract_user_credentials(
+            decoded_auth_header)
         if user_email is None or user_pwd is None:
             return None
 

78642c833a250aa9346b881869de76a2916cb19b
niniisg <nisselsanchez@gmail.com>
2025-02-23 15:00:23 -0400

    fixing pycdodestyle

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -34,10 +34,11 @@ class RedactingFormatter(logging.Formatter):
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str]):
-        super(RedactingFormatter, self).__init__(self.FORMAT)
+        super().__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
+        """Filter the record and redact specified fields."""
         return filter_datum(
             self.fields, self.REDACTION, super().format(record), self.SEPARATOR
         )

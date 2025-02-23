45e53f4da44ef7c5ec7cee85982b85bed185724e
niniisg <nisselsanchez@gmail.com>
2025-02-23 15:27:26 -0400

    fixing pycodestyle

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -42,6 +42,8 @@ class RedactingFormatter(logging.Formatter):
         return filter_datum(
             self.fields, self.REDACTION, super().format(record), self.SEPARATOR
         )
+
+
 def get_logger() -> logging.Logger:
     """function that returns a logging.Logger object"""
     logger = logging.getLogger("user_data")

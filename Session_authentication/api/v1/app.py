b78abbd0fe54afd45e285cd68f0d4c86a0a3ea7f
niniisg <nisselsanchez@gmail.com>
2025-02-23 23:31:04 -0400

    returning code

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -9,7 +9,7 @@ from flask_cors import CORS, cross_origin
 import os
 from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
-from api.v1.auth.session_auth import SessionAuth
+
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -25,9 +25,6 @@ if AUTH_TYPE == "basic_auth":
 elif AUTH_TYPE == "auth":
     auth = Auth()
 
-elif AUTH_TYPE == "session_auth":
-    auth = SessionAuth()
-
 
 @app.before_request
 def before_request():
@@ -38,8 +35,7 @@ def before_request():
                       "/api/v1/unauthorized/", "/api/v1/forbidden/"]
     if not auth.require_auth(request.path, excluded_paths):
         return
-    if auth.authorization_header(request) is None and \
-       auth.session_cookie(request) is None:
+    if auth.authorization_header(request) is None:
         abort(401)
     request.current_user = auth.current_user(request)
     if request.current_user is None:

ff4e30b46c565d97726a08fc282903900687a8f2
niniisg <nisselsanchez@gmail.com>
2025-02-23 21:17:09 -0400

    adding missin files

---
+++ Session_authentication/api/v1/auth/auth.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+"""
+Module for Authentication
+"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """
+    Base class for authentication
+    """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        Args:
+            Check if a path requires authentication
+        Returns:
+            False
+        """
+        if path is None:
+            return True
+        if excluded_paths is None or path not in excluded_paths == []:
+            return True
+
+        if not path.endswith("/"):
+            path += "/"
+
+        for excluded_path in excluded_paths:
+            if excluded_path.endswith("/") and path == excluded_path:
+                return False
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """
+        Gets the Authorization header from the request
+        Args:
+            request: the Flask request object
+        Returns:
+            None
+        """
+        if request is None:
+            return None
+        if "Authorization" not in request.headers:
+            return None
+        return request.headers.get("Authorization")
+
+    def current_user(self, request=None) -> TypeVar("User"):
+        """
+        Gets the current user from the request
+        Args:
+            the Flask request object
+        Returns:
+            None
+        """
+        return None

0126c85c137436503f9cf6b4dd9af8ad7def21f9
niniisg <nisselsanchez@gmail.com>
2025-02-27 20:50:29 -0400

    fixing pyco

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -31,7 +31,7 @@ class DB:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
-    
+
     def add_user(self, email: str, hashed_password: str) -> User:
         """
         Add a user to the database
@@ -41,4 +41,3 @@ class DB:
         self._session.commit()
 
         return user
-    

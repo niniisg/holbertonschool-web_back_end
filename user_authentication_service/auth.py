9356abcf0a2561fb94213e27fa9672830c2aa99c
niniisg <nisselsanchez@gmail.com>
2025-02-27 22:30:14 -0400

    fixing pyco and documentation

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python3
 
 """
+User Authentication
+service
 """
-
 import bcrypt
+
 
 def _hash_password(password: str) -> bytes:
     """
        Generate a salt using bcrypt
     """
-    
+
     salt = bcrypt.gensalt()
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
 

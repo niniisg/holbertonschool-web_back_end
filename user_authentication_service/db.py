55cc202c3a9fc5a0245a7890514cbffa862bf63b
niniisg <nisselsanchez@gmail.com>
2025-02-27 20:13:31 -0400

    fixing documentatioon

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -15,7 +15,8 @@ class DB:
     """
 
     def __init__(self) -> None:
-        """Initialize a new DB instance
+        """
+           Initialize a new DB instance
         """
         self._engine = create_engine("sqlite:///a.db", echo=True)
         Base.metadata.drop_all(self._engine)
@@ -24,7 +25,8 @@ class DB:
 
     @property
     def _session(self) -> Session:
-        """Memoized session object
+        """
+        Memoized session object
         """
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
@@ -32,9 +34,12 @@ class DB:
         return self.__session
     
     def add_user(self, email: str, hashed_password: str) -> User:
-        """Add a user to the database
+        """
+        Add a user to the database
         """
         user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
         self._session.commit()
+
         return user
+    

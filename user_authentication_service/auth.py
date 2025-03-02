20f98ec125573cd5a445ca44dc2056c810b86c41
niniisg <nisselsanchez@gmail.com>
2025-03-02 14:27:59 -0400

    fixing errors

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -86,6 +86,18 @@ class Auth:
         except NoResultFound:
             pass
 
+    def get_reset_password_token(self, email: str) -> str:
+        """Generate a reset password token for the user with the given email."""
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            raise ValueError("User does not exist")
+
+        reset_token = _generate_uuid()
+        self._db.update_user(user.id, reset_token=reset_token)
+        return reset_token
+
+
     def update_password(self, reset_token: str, password: str) -> None:
         """Update the user's password"""
         try:

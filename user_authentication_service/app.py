7fe50c8db8b4d0c9fd0e2594b632bde46fdf1ba3
niniisg <nisselsanchez@gmail.com>
2025-03-02 10:25:30 -0400

    fixing code error

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -15,8 +15,6 @@ def home():
     return jsonify({"message": "Bienvenue"})
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
 
 @app.route("/", methods=["POST"])
 def register_user() -> str:
@@ -33,7 +31,6 @@ def register_user() -> str:
         return jsonify({"email": email, "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
-
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")

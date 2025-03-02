03664f3fd96a3bd672a5448d4cd777022ed82e3a
niniisg <nisselsanchez@gmail.com>
2025-03-02 12:16:45 -0400

    credentials validation

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -38,3 +38,15 @@ class Auth:
             hashed_password = _hash_password(password)
             user = self._db.add_user(email, hashed_password)
             return user
+        
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """Validate the login credentials."""
+        try:
+            user = self._db.find_user_by(email=email)
+            if bcrypt.checkpw(password.encode('utf-8'), user.hashed_password):
+                return True
+            else:
+                return False
+        except NoResultFound:
+            return False

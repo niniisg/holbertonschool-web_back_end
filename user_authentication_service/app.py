3831527fb38e26980da0745f292e32392a9097dc
niniisg <nisselsanchez@gmail.com>
2025-03-02 13:19:20 -0400

    Log in

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -2,7 +2,7 @@
 """
 basic Flask app
 """
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort, make_response
 from auth import Auth
 
 
@@ -30,7 +30,24 @@ def register_user() -> str:
         return jsonify({"email": email, "message": "user created"}), 200
     except Exception:
         return jsonify({"message": "email already registered"}), 400
+    
+@app.route("/sessions", methods=["POST"])
+def login():
+    """Login user route"""
+    email = request.form.get("email")
+    password = request.form.get("password")
 
+    if not email or not password:
+        abort(401)
+
+    if not AUTH.valid_login(email, password):
+        abort(401)
+
+    session_id = AUTH.create_session(email)
+    response = make_response(jsonify({"email": email, "message": "logged in"}))
+    response.set_cookie("session_id", session_id)
+
+    return response
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")

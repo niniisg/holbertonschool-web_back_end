fdfe49356d9f1d9c953b39b2dc01ff6bbfed0bc1
niniisg <nisselsanchez@gmail.com>
2025-03-22 09:14:43 -0400

    Reading from Redis

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -26,3 +26,28 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+        
+    def get(self, key: str, fn: Optional[Callable] = None) -> Any:
+        """
+        Get data from Redis and convert it using the callable fn
+        """
+        data = self._redis.get(key)
+        if data is None:
+            return None
+        
+        if fn:
+            return fn(data)
+        return data
+    
+    def get_str(self, key: str) -> str:
+        """
+        Get a string value from Redis
+        """
+        return self.get(key, lambda d: d.decode("utf-8"))
+    
+    def get_int(self, key: str) -> int:
+        """
+        Get an integer value from Redis
+        """
+        return self.get(key, int)
+    

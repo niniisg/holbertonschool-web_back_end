fbd5d0cb84c1e2e00a6a97223824a9315b0502a1
niniisg <nisselsanchez@gmail.com>
2025-03-22 11:45:11 -0400

    Retrieving lists

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -41,9 +41,25 @@ def call_history(method: Callable) -> Callable:
         return result
     return wrapper
 
-
-
-
+def replay(method: Callable):
+    """
+    Display the history of calls of a particular function
+    """
+    redis_instance = method.__self__._redis 
+    method_name = method.__qualname__
+    
+    calls_count = redis_instance.get(method_name)
+    calls_count = calls_count.decode("utf-8") if calls_count else "0"
+    
+    print(f"{method_name} was called {calls_count} times:")
+    
+    inputs = redis_instance.lrange(f"{method_name}:inputs", 0, -1)
+    outputs = redis_instance.lrange(f"{method_name}:outputs", 0, -1)
+    
+    for input_data, output_data in zip(inputs, outputs):
+        input_str = input_data.decode("utf-8")
+        output_str = output_data.decode("utf-8")
+        print(f"{method_name}(*{input_str}) -> {output_str}")
 
 class Cache:
     """
@@ -89,3 +105,5 @@ class Cache:
         Get an integer value from Redis
         """
         return self.get(key, int)
+    
+  

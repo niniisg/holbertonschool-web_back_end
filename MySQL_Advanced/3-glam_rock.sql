e1d5e6ae763b7e2e787e9a4ad28a5b6c5041ed0b
niniisg <nisselsanchez@gmail.com>
2025-03-22 21:18:30 -0400

    old school band

---
+++ MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,5 @@
+-- Lists all Glam rock bands, ranked by their longevity
+SELECT band_name, (IFNULL(split, 2022) - formed) AS lifespan
+FROM metal_bands
+WHERE style LIKE '%Glam rock%'
+ORDER BY lifespan DESC;

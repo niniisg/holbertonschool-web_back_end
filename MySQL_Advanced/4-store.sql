d7ad0b8a7c2b6f442a7cc3980a6947d22b5e17f9
niniisg <nisselsanchez@gmail.com>
2025-03-22 21:34:16 -0400

    task 4

---
+++ MySQL_Advanced/4-store.sql
@@ -0,0 +1,13 @@
+-- Creates a trigger to decrease the quantity of an item
+DELIMITER //
+
+CREATE TRIGGER decrease_quantity
+AFTER INSERT ON orders
+FOR EACH ROW
+BEGIN
+    UPDATE items
+    SET quantity = quantity - NEW.number
+    WHERE name = NEW.item_name;
+END //
+
+DELIMITER ;
